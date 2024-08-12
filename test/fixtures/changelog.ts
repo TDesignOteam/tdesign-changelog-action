2cb5dd7e31a668a628321dac2651d1eefd64f852
黎伟杰 <674416404@qq.com>
2024-08-12 21:37:17 +0800

    chore: add fixtures

---
+++ test/fixtures/changelog.ts
@@ -0,0 +1,15 @@
+export const changelog = `
+- fix(A): a bug fix
+- hotfix(A): a bug fix
+- bug(A): a bug fix
+- feat(B): a feature
+- feature(B): a feature
+- enhancement(B): a feature
+- break(C): a major update
+- breaking(C): a major update
+- feat(D): a feature
+- docs(): a doc change
+- chore: a chore
+- break(g): a minor fix
+- break(H H): a major update
+`
--- test/renderer.test.ts
+++ test/renderer.test.ts
@@ -1,23 +1,9 @@
 import { describe, expect, it } from 'vitest'
 import { ChangelogReg } from '../src/renderer'
+import { changelog } from './fixtures/changelog'
 
 describe('renderer', () => {
   it('changelogReg', () => {
-    const changelog = `
-- fix(A): a bug fix
-- hotfix(A): a bug fix
-- bug(A): a bug fix
-- feat(B): a feature
-- feature(B): a feature
-- enhancement(B): a feature
-- break(C): a major update
-- breaking(C): a major update
-- feat(D): a feature
-- docs(): a doc change
-- chore: a chore
-- break(g): a minor fix
-- break(H H): a major update
-`
     const result = changelog.matchAll(ChangelogReg)
     expect([...result]).toMatchSnapshot()
   })
