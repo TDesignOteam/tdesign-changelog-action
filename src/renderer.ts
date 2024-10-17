35b86afb08d289b114927ab8a2f6a3f888bbd578
liweijie0812 <674416404@qq.com>
2024-10-17 22:22:12 +0800

    chore: build dist (#38)
    * chore: build dist
    
    * fix: CHANGELOG_REG
    
    * chore: build

--- src/renderer.ts
+++ src/renderer.ts
@@ -8,7 +8,7 @@ const featureLabel = ['feature', 'feat', 'enhancement']
 const docsLabel = ['docs', 'doc', 'documentation']
 const refactorLabel = ['pref', 'refactor']
 
-export const CHANGELOG_REG = /-\s([A-Z]+)(?:\(([A-Z\s]*)\))?\s*:\s(.+)/gi
+export const CHANGELOG_REG = /-\s([A-Z]+)(?:\(([A-Z\s_-]*)\))?\s*:\s(.+)/gi
 export const PULL_NUMBER_REG = /in\shttps:\/\/github\.com\/.+\/pull\/(\d+)/g
 export const SKIP_CHANGELOG_REG = /\[x\] 本条 PR 不需要纳入 changelog/i
 export function getPullNumbers(body: string) {
