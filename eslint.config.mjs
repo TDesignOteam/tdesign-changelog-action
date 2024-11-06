d8bcc891e1d8de48d0005e9921e1371f407257c7
黎伟杰 <674416404@qq.com>
2024-11-06 14:42:23 +0800

    chore: eslint use formatters

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,5 @@
 import antfu from '@antfu/eslint-config'
 
 export default antfu({
-  // Enable stylistic formatting rules
-  // stylistic: true,
-
-  // Or customize the stylistic rules
-  stylistic: {
-    indent: 2, // 4, or 'tab'
-    quotes: 'single', // or 'double'
-  },
-  yaml: true,
-  typescript: true,
+  formatters: true,
 })
